ff2ba4969562e1631fae8c4f3671b31f4392774a
natcalderon <nat.calderon04@gmail.com>
2021-04-08 20:19:08 -0600

    quiz html starter

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// When start button is clicked
+var startBtn = document.querySelector('#start-btn')
+
+
+// Timer begins
+// First question pops up
+// If question is answerd, new question pops up
+// if quetion is incorrect, minus time from timer
+// If all questions are answered, game is over
+// If timer hits 0, game is over
+// When game is over, initial prompt
